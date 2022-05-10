8962d8e0c50976c42cbb62c1ff640d040b7151f2
jlemke <jlemke3@madisoncollege.edu>
2022-05-10 18:25:33 -0500

    Added reducer hook to fetch notes

--- src/App.js
+++ src/App.js
@@ -6,23 +6,77 @@ import { List } from 'antd';
 import 'antd/dist/antd.css';
 import { listNotes } from './graphql/queries';
 
+
+
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+
+    case 'SET_NOTES':
+      return { 
+        ...state, 
+        notes: action.notes, 
+        loading: false 
+      };
+
+    case 'ERROR':
+      return { 
+        ...state, 
+        loading: false, 
+        error: true 
+      };
+
+    default:
+      return { 
+        ...state
+      };
+  }
+};
+
+
+
 const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNotes = () => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      });
+
+      dispatch({ 
+        type: 'SET_NOTES',
+        notes: notesData.data.listNotes.items
+      });
+    } 
+    catch (err) {
+      console.log('error: ', err);
+      dispatch({ 
+        type: 'ERROR'
+      });
+    }
+  };
+
+  useEffect(
+    () => {
+      fetchNotes();
+    }, []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 };
