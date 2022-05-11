5ed24cedf2686be343a1aa5273dddcdb78bd5100
jlemke <jlemke3@madisoncollege.edu>
2022-05-11 13:39:45 -0500

    Centered the div...
    Managed to get completed / total centered while ignoring whether the checkmark is there or not.

--- src/InfoBar.js
+++ src/InfoBar.js
@@ -1,22 +1,42 @@
-
+import { CheckCircleOutlined } from "@ant-design/icons";
 
 export const InfoBar = ({notes}) => {
 
     const completed = notes.reduce((acc, note) => note.completed ? acc + 1 : acc, 0);
 
     return (
-        <div>
-            <hr/>
-            <h3 style={styles.infobar}>
-                { completed } completed / { notes.length } total
-            </h3>
-            <hr/> 
-        </div>
+        <>
+            <hr style={styles.hr} />
+            <div style={styles.container}>
+                
+                <h3 style={styles.infobar}>
+                    { completed } completed / { notes.length } total
+                </h3>
+                
+                    {completed === notes.length && 
+                        <div style={styles.checkMark}>
+                            <CheckCircleOutlined />
+                        </div>
+                    }
+                
+            </div>
+            <hr style={styles.hr} /> 
+        </>
     );
 };
 
 const styles = {
+    container: {
+        display: "flex",
+        justifyContent: "center",
+        width: "100%"
+    },
     infobar: {
-        textAlign: "center"
+        marginRight: 10,
+        marginBottom: 0
+    },
+    checkMark: {
+        color: "#00ffff",
+        width: 0
     }
 };
